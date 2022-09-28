b9f485115de92c0ca5b9510f9b6fb99e4490103f
Daniel <daniel.lima@inmetrics.com.br>
2022-09-28 15:00:19 +0200

    Adding Ridge and Lasso

--- project1/LASSO.py
+++ project1/LASSO.py
@@ -1,9 +1,17 @@
+from sklearn import linear_model
 
 
 class LASSO():
 
-    def beta_lasso(self):
+    def beta_lasso(self, design, known, lmbd):
         '''
-        Makes LASSO estimator.
+        Makes LASSO estimator. Here we will use the sklearn Lasso model in an unsual way.
+        Instead of getting the prediction right away, we are going to return only the beta.
         '''
-        pass
+
+        clf = linear_model.Lasso(alpha=lmbd)
+        clf.fit(design, known)
+
+        beta = clf.coef_
+
+        return beta
