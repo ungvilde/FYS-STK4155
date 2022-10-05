046d49970d50d80802e83446ed070de7d31a717e
Nils T <n.e.c.taugbol@fys.uio.no>
2022-10-05 12:08:44 +0200

    Commented the code and added documentation

--- project1/LinearRegression.py
+++ project1/LinearRegression.py
@@ -20,99 +20,145 @@ class LinearRegression(OLS, LASSO, Ridge):
     '''
 
     # the 'private' variables for the superclass
-    _beta = None
-    _method = None
-    _design = None
-    _order = None
-    _x = None
-    _y = None
-    _z = None
-    _shape = None
-    _fit = None
-    _lambda = None
+    _beta = None    # contains the estimator
+    _method = None  # keeping track of the method used in the instance (ols, ridge or lasso)
+    _design = None  # conatins the design matrix
+    _order = None   # keeping track of the order of the polynomial fit
+    _x = None   # self explanatory
+    _y = None   # self explanatory
+    _z = None   # self explanatory
+    _fit = None # contains the most recent fit done
+    _lambda = None  # contains the lambdas used for ridge or lasso
 
 
     def __init__(self, order, x, y, z, method=1, lmbd=None):
-        '''Creates a regression object that will make a linear regression of a chosen order.  Methods are OLS: 1, Ridge: 2 and LASSO: 3'''
+        '''Creates an instance of LinearRegression which will do either OLS, Ridge or LASSO depending on the method number chosen.
+        
+        Parameters
+        ----------
+        order : int
+            order of the polynomial fit. The initialization of the object will create a design matrix for the chosen order.
+
+        x, y, z : numpy arrays
+            x and y should be be meshgrids. That is a consequence of hoe the class was built.
+        
+        method : int (1, 2 or 3)
+            Default 1\n
+            Chosen method for linear regression. 1 is OLS, 2 is Ridge and 3 is LASSO.
+        
+        lmbd : float
+            Default None\n
+            Lambda to which we will make a fit for either Ridge or LASSO
+        
+        Raises
+        ------
+        OutOfBounds(Exception)
+            if method chosen is not 1, 2 or 3
+        '''
+
+        # setting the parameters that are chosen for this instance adn storing them in the object
         self._order = order
         self._z = z
         self._x = x
         self._y = y
-        self._shape = np.shape(z)
         self._lambda = lmbd
 
+        # checking if chosen method is valid
         if method not in [1, 2, 3]:
             raise OutOfBounds()
 
         self._method = method
 
-        self.design()
+        self.design()   # calculating the design matrix immediately
 
     def __call__(self):
-        '''Makes a fit of chosen order.'''
-        self.beta()
+        '''
+        Makes a fit of chosen order.
+        
+        Calculates the beta with the chosen method and makes a fit
 
-        self._fit = self._design @ self._beta
-        self._fit = np.reshape(self._fit, self._shape)
+        Returns
+        -------
+        The polynomial fit.
+        '''
+        self.beta() # calculates the beta
+
+        self._fit = self._design @ self._beta   # makes the fit
+        self._fit = np.reshape(self._fit, np.shape(self._z))    # reshapes to match the z
 
         return self._fit
     
     def predict_resample(self, design_train, z_train, design_test):
-        self._design = design_train
-        self._z = z_train ### IS THIS CORRECT??? SHOULD IT NOT BE self._z = z_test????
-        self.beta()
+        '''
+        Prediction method for the resampling method (see Resampling class). Takes the training data and the test data. Makes a design matrix and an estimator using the training data and then a fit using the test data. Returns that fit.
 
-        self._fit = design_test @ self._beta
+        Parameters
+        ----------
+        design_train : numpy array (matrix)
+            design matrix for the training data. The size and shape already decides the polynomial degree.
+        
+        z_train : numpy array
+            known training output used to create the beta.
+        
+        design_test : numpy array (matrix)
+            design matrix from the test data which will be used to calculate the fit.
+        
+        Returns
+        -------
+        The fit (1D) of the design_test @ beta, where the beta is calculated from the training parameters.
+        '''
+        self._design = design_train # sets the instance designmatrix to be the training design matrix
+        self._z = z_train   # sets the known output for the training data which is used to calculate the estimtor based off of the training set
+        self.beta() # calculates the beta using the training data-set
 
-        return self._fit
+        self._fit = design_test @ self._beta    # makes a fit on the test-data
+
+        return self._fit    # returns the fit
 
     def predict(self, design_test, z_test):
-        temp = self._z
-
-        self._z = z_test
-        self._design = design_test
-        shape = np.shape(z_test)
-        self._fit = self._design @ self._beta
-        self._fit = np.reshape(self._fit, shape)
-        out_mse = self.mse()
-        out_r2 = self.r2()
-        
-        self._z = temp
-
-        return out_mse, out_r2
-
-    def predict_xy(self, x, y, z, beta = None):
         '''
-        Tests the model with new (test) x and y and compares it with a different (test) z.
-        '''
-        self._x = x
-        self._y = y
-        temp = self._z
-        self._z = z
-        self.design()
-        N = len(x[0, :])
+        Basic predicting of test data with an already computed beta, evaluates the model. The instance's stored z is not replaced for consistency (the testing z and design are used but not stored).
 
-        if type(beta) == np.ndarray:
-            own = self._design @ beta
-            own = np.reshape(own, (N, N))
+        Needs
+        -----
+        An already calculated beta.
 
-            out_mse = self.mse(own=own)
-            out_r2 = self.r2(own=own)
-
-        else:
-            self._fit = self._design @ self._beta
-            self._fit = np.reshape(self._fit, (N, N))
-
-            out_mse = self.mse()
-            out_r2 = self.r2()
+        Parameters
+        ----------
+        design_test : numpy array (matrix)
+            design matrix of the test data which will be used to make a fit.
         
-        self._z = temp
+        z_test : numpy array
+            the fit which we will test against
+        
+        Returns
+        -------
+        The MSE and R2-score of the predicted fit.
+        '''
+        temp = self._z  # saves the z of the instance
+
+        self._z = z_test    # sets the testing z
+        self._design = design_test  # sets the testing design matrix
+
+        shape = np.shape(z_test)    # so that we are comparing the same shapes after 
+        self._fit = self._design @ self._beta   # makes the fit with an already calculated beta
+        self._fit = np.reshape(self._fit, shape)    # reshapes the fit to be the same shape as z_test
+
+        out_mse = self.mse()    # evaluates the fit
+        out_r2 = self.r2()  # evaluates the fit
+        
+        self._z = temp  # resetting the z for consistency
 
         return out_mse, out_r2
 
     def design(self):
-        '''with the x and y parameters as well as the maximum order of the fit computes a 
-        designmatrix that takes in all the permutations of x and y up the chosen order'''
+        '''
+        Makes the design matrix based off of the chosen order and the x and y data. Stores the design matrix within the object, to get it use get_design().
+
+        Needs
+        -----
+        x and y data. A chosen order for the polynomial fit which can be updated through set_order(int(order)).
+        '''
 
         # for the franke function we see that x and y are part of a meshgrid
         # to work with this we need to ravel them
@@ -129,7 +175,7 @@ class LinearRegression(OLS, LASSO, Ridge):
         # keeping track of where to fill values
         place = 1
 
-        # filling in the design matrix with the different permutations of x and y (binomial style?)
+        # filling in the design matrix with the different permutations of x and y (binomial style)
         for i in range(1, self._order+1):
             for ii in range(i+1):
                 X[:, place] = x**ii * y**(i-ii)
@@ -138,128 +184,281 @@ class LinearRegression(OLS, LASSO, Ridge):
         self._design = X
     
     def beta(self):
+        '''
+        Calculates the estimator with the chosen method. Calls upon the inherited beta calculator from the OLS, Ridge or LASSO classes. Returns the beta though that might not be necessary with a get_beta() method.
+
+        Needs
+        -----
+        A design matrix and a known z stored in the instance. Meaning you must have set them when the instance was created.
+        '''
 
         if self._method == 1:
-            self._beta = self.beta_ols(self._design, self._z)
+            self._beta = self.beta_ols(self._design, self._z)   # calculates the beta with the chosen method
+
         elif self._method == 2:
-            self._beta = self.beta_ridge(self._design, self._z, self._lambda)
+            self._beta = self.beta_ridge(self._design, self._z, self._lambda)   # calculates the beta with the chosen method
+
         else:
-            self._beta = self.beta_lasso(self._design, self._z, self._lambda)
+            self._beta = self.beta_lasso(self._design, self._z, self._lambda)   # calculates the beta with the chosen method
+
         return self._beta
 
     # VARIANCE AND SUCH
 
     def mse(self, own=None):
-        '''takes in the solution and the approximation and calculates the mean squared error of the fit'''
+        '''
+        Calculates the MSE using the Errors class.
 
-        error = Errors(self._z)
+        Needs
+        -----
+        Needs to have made a fit and have it stored in the instance as well as a knwon z to ompare to.
 
-        if type(own) == np.ndarray:
+        Parameters
+        ----------
+        own : numpy array
+            Default None\n
+            a chosen fit to which to compare to.
+
+        Returns
+        -------
+        The calculated MSE as a float
+        '''
+
+        error = Errors(self._z) # creates the Error object and sets the knwon z we will compare to
+
+        if type(own) == np.ndarray: # compares to the chosen fit
             error.set_fit(own)
             return error.mse()
         else:
-            error.set_fit(self._fit)
+            error.set_fit(self._fit)    # compares to the fit stored in the instance
             return error.mse()
 
     def r2(self, own=None):
-        '''takes in the solution and the approximation and calculates the R2 score of the fit'''
+        '''
+        Calculates the R2 score using the Errors class.
 
-        error = Errors(self._z)
+        Needs
+        -----
+        Needs to have made a fit and have it stored in the instance as well as a knwon z to ompare to.
+
+        Parameters
+        ----------
+        own : numpy array
+            Default None\n
+            a chosen fit to which to compare to.
+
+        Returns
+        -------
+        The calculated R2 score as a float
+        '''
+        error = Errors(self._z) # creates the Error object and sets the knwon z we will compare to
     
-        if type(own) == np.ndarray:
+        if type(own) == np.ndarray: # compares to the chosen fit
             error.set_fit(own)
             return error.r2()
         else:
-            error.set_fit(self._fit)
+            error.set_fit(self._fit)    # compares to the fit stored in the instance
             return error.r2()
    
     def var_beta(self):
-        '''can only get var of ols estimator here'''
+        '''
+        This method is only valid if the chosen method for the fitting is OLS! Calculates the variance of the estimator through the properties given by the linalg method of find the estimator.
 
-        if self._method != 1:
+        Needs
+        -----
+        to be an instance that has method == 1\n
+        to have made a fit.
+
+        Returns
+        -------
+        the variance of the estimator : numpy array
+
+        Raises
+        ------
+        OutOfBounds(Exception) if the chosen method is not OLS
+        '''
+
+        if self._method != 1:   # checks if the method is OLS
             raise OutOfBounds(var_beta=True)
         
         else:
-            variance_ols = Errors(self._z, self._fit)
+            variance_ols = Errors(self._z, self._fit)   # creates an instance of Errors to then find the variance of beta
 
-            return variance_ols.var_beta_ols(self._beta, self._design)
+            return variance_ols.var_beta_ols(self._beta, self._design)  # calculates and returns
 
     def conf_int(self):
-        '''can only get var of ols estimator here'''
+        '''
+        This method is only valid if the chosen method for the fitting is OLS! Calculates the confidence intervals of the estimator through the properties given by the linalg method of find the estimator.
 
-        if self._method != 1:
+        Needs
+        -----
+        to be an instance that has method == 1\n
+        to have made a fit.
+
+        Returns
+        -------
+        The confidence intervals of the estimator : numpy array
+
+        Raises
+        ------
+        OutOfBounds(Exception) if the chosen method is not OLS
+        '''
+
+        if self._method != 1:   # checks that the method is OLS
             raise OutOfBounds(conf_int=True)
         
         else:
-            conf_intervals = Errors(self._z, self._fit)
+            conf_intervals = Errors(self._z, self._fit) # creates an instance of Errors
 
-            return conf_intervals.conf_int(self._beta, self._design)
+            return conf_intervals.conf_int(self._beta, self._design)    # finds and returns the confidence intervals of the estimator
 
     def split_predict_eval(self, test_size=0.2, fit=False, train=False, random_state=None, scale=False):
-        """
-        The default is to use the mse from the test, but if train is true, the calculated mse is from train!
-        """
-        if scale:
+        '''
+        Splits the contained data into training and testing and either returns the testing data and stores the training data or makes a fit and evaluates it.
+
+        Needs
+        -----
+
+        Parameters
+        ---------
+        test_size : float (between 0 and 1)
+            decides the fraction which will make up the test data from the original data.
+        
+        fit : boolean
+            Default False\n
+            Flag for choosing if it fits and evaluates (True) or not (False)
+        
+        train : boolean
+            Default False\n
+            If True the returned mse and r2 will be evaluated from the training data and not the test.
+        
+        random_state : int
+            Default None\n
+            Seed from which to randomize the splitting of the data.
+        
+        scale : boolean
+            Default False\n
+            Flag for scaling (True) or not (False) the design matrix.
+        '''
+
+        if scale:   # normalizes the design matrix
             self._design = normalize(self._design)
 
         X_train, X_test, z_train, z_test = our_tt_split(self._design, self._z, test_size=test_size, random_state=random_state)
 
+        # setting the training data to be stored in the instance and calculates the beta from it
         self._design = X_train
         self._z = z_train
         self._beta = self.beta()
 
         if fit:
-            if train:
-                self._predict = self.predict(X_test, z_test)
+            if train: # evaluates the training data
                 # this returns mse and r2
                 return self.predict(X_train, z_train)
-            else:
-                # then it should gibe the test data metrics
-                self._predict = self.predict(X_test, z_test)
+
+            else: # evaluates the model on the testing data
                 # this returns mse and r2
                 return self.predict(X_test, z_test)
+
         else:
             return X_test, z_test
-
-    def split_xy(self, t_size=0.2):
-        """
-        !!! is this necessary????
-        """
-        x = np.ravel(self._x)
-        y = np.ravel(self._y)
-        z = np.ravel(self._z)
-
-        return train_test_split(x, y, z, test_size=t_size)
 
 # GET and SET
 
     def set_beta(self, design, z):
-        '''Sets a design matrix and calculates beta.'''
+        '''
+        Sets a design matrix and the known output and calculates beta. All of the parameters will be stored in the object.
+
+        Needs
+        -----
+        It doesn't need anything but remember that it will calulate a beta using the chosen method and that the order of the polynomial fit is now decided by the design matrix.
+        
+        Parameters
+        ----------
+        design : numpy array (matrix)
+            sets the new design matrix which the object will use
+        
+        z : numpy array
+            sets the new z which the object will use
+        
+        Returns
+        -------
+        Nothing
+        '''
         self._design = design
         self._z = z
-        self.beta()
+        self.beta() # calculates and stores the new estimator in the object
 
     def set_known(self, z):
+        '''
+        Sets a new z.
+
+        Parameters
+        ----------
+        z : numpy array
+        '''
         self._z = z
 
     def set_order(self, order):
+        '''
+        Unsure if used!\n
+        Sets a new order for the polynomial, this will not do anything unless you calculate a new design matrix.
+
+        Parameters
+        ----------
+        z : numpy array
+        '''
         self._order = order
     
     def set_design(self, design):
+        '''
+        Unsure if used!\n
+        Sets a new design matrix.
+
+        Parameters
+        ----------
+        design : numpy array (matrix)
+        '''
         self._design = design
 
     def get_beta(self):
-
+        '''
+        Returns
+        -------
+        The stored estimator : numpy array
+        '''
         return self._beta
 
     def get_design(self):
+        '''
+        Returns
+        -------
+        The stored design matrix : numpy array (matrix)
+        '''
         return self._design
     
     def get_known(self):
+        '''
+        Returns
+        -------
+        The stored z : numpy array
+        '''
         return self._z
     
     def get_x(self):
+        '''
+        Unsure if used!\n
+        Returns
+        -------
+        The stored x values : numpy array
+        '''
         return self._x
     
     def get_y(self):
+        '''
+        Unsure if used!\n
+        Returns
+        -------
+        The stored y values : numpy array
+        '''
         return self._y
